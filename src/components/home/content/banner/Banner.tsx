f8f8313f1263a06e5dd91552b2d2d5394e59b2a5
Douglas <dodo_nadoti@hotmail.com>
2023-01-08 23:06:57 -0300

    puxando img do banner

--- src/components/home/content/banner/Banner.tsx
+++ src/components/home/content/banner/Banner.tsx
@@ -1,24 +1,36 @@
 import React from 'react'
 
 export function Banner() {
-  let api = '&api_key=fe711924d01611d5e183b05b5010b8e7'
+  let url_key = '&api_key=fe711924d01611d5e183b05b5010b8e7'
   let url = 'https://api.themoviedb.org/3'
 
   let url_img = 'https://image.tmdb.org/t/p/w500/'
 
-  const [img, setImg] = React.useState([])
+  const [listImgBanner, setListImgBanner] = React.useState([])
+  const [selectedImgBanner, setSelectedImgBanner] = React.useState({})
+
+
+  async function takeMoviePolular() {
+    await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=fe711924d01611d5e183b05b5010b8e7&language=en-US&page=1`)
+      .then(res => res.json())
+      .then(json => {
+        const takeThree = json.results.slice(0, 3)
+        setListImgBanner(takeThree)
+        console.log(takeThree)
+      })
+  }
+
   React.useEffect(() => {
-    async function teste() {
-      await fetch(`${url}/discover/movie?sort_by=popularity.desc${api}`)
-        .then(res => res.json())
-        .then(json => console.log(json))
-    }
-    teste()
+
+    takeMoviePolular()
   }, [])
+
   return (
     <section >
       <div>
-
+        {listImgBanner?.map(item => (
+          <img key={item.id} src={`${url_img}${item.backdrop_path}`} alt="" />
+        ))}
       </div>
 
       <div>
